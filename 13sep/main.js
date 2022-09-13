0a058d8f8c660c1aa272f6b9c26df078a2428d3d
gloria bello <calderonscarlette69@gmail.com>
2022-09-13 14:40:45 -0300

    creamos archivos indexcon info de java

---
+++ 13sep/main.js
@@ -0,0 +1,40 @@
+// declarar 4 variables 1 costante
+let name = "gloria"
+let lastname = "bello"
+let age = 22
+let toy = "football"
+const height = 1.60 
+
+// tipo de datos
+let flag = true // boolean , dicotomica
+let number = 1 // number, numeros enteros o decimales
+let text = 'el texto va entre comillas' // string
+
+// arrays, 
+// cada elemento tiene un indice y parten desde 0
+let arr = [1, 3, 4, 8] // arrays 
+let arr2 = ["texxto", [1,2,3], 4, true] // arrays
+
+arr[2] // acceder al valor 4 
+arr[2] = 5 //asignando un nuevo valor 
+
+// variableName [index] -> acceder al valor en un arreglo 
+
+
+let nothing = null // null representa un valor "vacio" o "nulo"     
+let nodef = undefined 
+let width // sin un valor asignado, esta variable es "undefined"
+// undefined suele no usarse, ya que javascrip lo hace
+
+// estructuras de control fijo
+if 
+else if
+
+// el if va a ser algo si se cumple la condicion
+// else va a ejecutar algo cuando la condicion de if de falso 
+
+
+
+
+ 
+ 
